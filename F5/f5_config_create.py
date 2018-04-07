5d5201918f90793a9ef6faf21d18c364489ecbc3
Travis <t@tburton.net>
2018-04-07 14:40:52 -0400

    cleanup

--- F5/f5_config_create.py
+++ F5/f5_config_create.py
@@ -1,7 +1,7 @@
 ##!/usr/bin/python3.6.3
 
 import socket
-# Create dictionary 
+ 
 mydict = {}
 inputpath = input('Provide path to input file (list of domain names): ')
 namefile = open(inputpath,"r")
@@ -10,7 +10,6 @@ for name in namefile:
     print('Checking ' + name)
     ip = socket.gethostbyname(name.strip())
     mydict.update({name.replace('.flexnet.net', '').strip().upper(): ip})
-    #print(name + ip)
 
 namefile.close()
 
